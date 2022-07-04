a02f6101172bfe87232b9d170aedf0002ae93cf4
brunosales7 <bruno_sales7@hotmail.com>
2022-07-04 11:58:36 -0300

    Hello Word

--- CursoGitHub.InterfaceConsole/Program.cs
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
 namespace CursoGitHub.InterfaceConsole
+    //inicio do namespace
 {
     internal class Program
-    {
+    {//inicio da classe
         static void Main(string[] args)
-        {
-            Console.WriteLine("Alô Mundo!");
+        { //inicio do método
+            /*
+             * Main - principal
+             * O método inicia-se com a letra maiscula
+             * Variável
+             * É um espaço reservado na memória
+             */
+            string pergunta;
+            string resposta="git init";
+            Console.WriteLine("Qual comando para iniciar o git?");
+            pergunta = Console.ReadLine();
+            if (pergunta.Equals(resposta))
+            {
+                Console.WriteLine("acertou");
+            }
+            else
+            {
+                Console.WriteLine("errou");
+            }
+            Console.WriteLine($"A resposta é : {resposta}");
+            Console.WriteLine(resposta);
+            resposta = Console.ReadLine();
+
+//            Console.WriteLine("Alô Mundo!");
         }
-    }
-}
+    }//fim do método
+}//fim da classe
+//fim do namespace
